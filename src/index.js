5f9008dd2fea4b25616cd64d6633b9145f2f0e63
DmitriyDS <70814879+DmitriyDS91@users.noreply.github.com>
2020-10-03 17:01:15 +0500

    feat: add max, min and average functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array == 0) return 0;
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+    array.sort(compareNumbers);
+    return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array == 0) return 0;
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+    array.sort(compareNumbers);
+    return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array == 0) return 0;
+    let sum = 0;
+    for (let i = 0; i <= array.length - 1; i++) sum = sum + array[i];
+    return sum / array.length;
 }
